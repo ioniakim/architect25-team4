5566824ffbfdc81a5c4b5aa520c8bb068e45d7bc
Sung Woo Kim <ionia.kim@samsung.com>
2025-06-07 10:33:27 +0000

    refactor gemini_mcp_agent_client in form of mcp_agent_client

--- agents/gemini_mcp_agent_client.py
+++ agents/gemini_mcp_agent_client.py
@@ -1,5 +1,5 @@
 import asyncio
-from typing import List, Optional
+from typing import List, Optional, get_type_hints
 
 # New imports for creating a StructuredTool
 from pydantic import BaseModel, Field
@@ -9,38 +9,66 @@ from langchain_core.messages import SystemMessage, HumanMessage
 from langchain_core.tools import BaseTool
 from langchain_mcp_adapters.client import MultiServerMCPClient
 from langgraph.prebuilt import create_react_agent
+from langchain_core.language_models.chat_models import BaseChatModel
 from langchain_google_genai import ChatGoogleGenerativeAI
 
 
-async def create_mcp_react_agent(config: dict, llm):
+def get_agent_client(config: dict, llm: BaseChatModel=None) -> BaseTool:
     """
-    Creates a ReAct agent using its default, built-in prompt.
+    Creates an agent client according to config.
+    """
+    if llm is None:
+        raise RuntimeError("The argument LLM is None")
+    with asyncio.Runner() as runner:
+        return runner.run(_create_agent(config, llm))
+
+async def _create_agent(config: dict, llm) -> BaseTool:
+    """
+    Create an react agent and turn it into an agent as tool.
     """
     name = config["name"]
+    description = config["description"]
     mcp_config = config["mcp"]
+    print("###########################")
+    print(f"Creating an agent client {name}")
+    print("###########################")
 
-    client = MultiServerMCPClient({
-        name: {
-            "url": mcp_config["url"],
-            "transport": mcp_config.get("transport", "streamable-http")
-        },
-    })
-
-    tools = await client.get_tools()
-
-    print(f"Successfully fetched {len(tools)} tools for agent: {name}")
-    for tool in tools:
-        print(f"\t- Tool: {tool.name}, Description: {tool.description}")
-
-    agent_executor = create_react_agent(
-        model=llm,
-        tools=tools,
-    )
+    # 1. Get tools
+    print("###########################")
+    print(f"Retrieving tools from MCP Server {name}")
+    print("###########################")
+    tools = await _get_tools(name, mcp_config)
     
-    return agent_executor
+    # 2. Generate prompt
+
+    print("###########################")
+    print(f"Generating the prompt for the agent {name}")
+    print("###########################")
+    prompt = _generate_prompt_for_tools(tools)
+    print(f"\nAgent Prompt: \n{prompt}")
+
+    # 3. Create an react agent
+    print("###########################")
+    print(f"Creating an react agent {name}")
+    print("###########################")
+    agent = create_react_agent(model=llm, tools=tools, prompt=prompt)
+
+    # 4. Create an agent as a tool
+    print("###########################")
+    print(f"Creating an agent as a tool {name}")
+    print("###########################")
+    agent_tool = _create_agent_as_tool(agent, system_prompt=prompt, tool_name= name, tool_description=description)
+
+    print("###########################")
+    print(f"Created an agent client {name}")
+    print("###########################")
+    print(f"Tool Name: {agent_tool.name}")
+    print(f"Tool Description: {agent_tool.description}")
+    print(f"Tool Args: {list(agent_tool.args.keys())}")
+    return agent_tool
 
 
-def create_agent_as_tool(agent_executor, system_prompt: str, tool_name: str, tool_description: str) -> StructuredTool:
+def _create_agent_as_tool(agent_executor, system_prompt: str, tool_name: str, tool_description: str) -> StructuredTool:
     """
     Wraps a LangGraph agent executor in a StructuredTool.
     """
@@ -50,7 +78,7 @@ def create_agent_as_tool(agent_executor, system_prompt: str, tool_name: str, too
         input: str = Field(description="A detailed, natural language query for the agent.")
 
     # Define the tool function
-    async def run_agent(input: str, context: Optional[list[str]] = []) -> str:
+    async def run_agent(input: str) -> str:
         """The async function that will be executed when the tool is called."""
 
         agent_input = {
@@ -60,7 +88,7 @@ def create_agent_as_tool(agent_executor, system_prompt: str, tool_name: str, too
             ]
         }
 
-        print(f"\n\nFunction Call:\n\tinput={input}, context={context}")
+        print(f"\n\nFunction Call:\n\tinput={input}")
         result = await agent_executor.ainvoke(agent_input)
         print(f"Function Call: result: {result}")
         return result['messages'][-1].content
@@ -74,8 +102,8 @@ def create_agent_as_tool(agent_executor, system_prompt: str, tool_name: str, too
         args_schema=SubAgentInput
     )
 
-async def create_agent(config: dict, llm=None) -> BaseTool:
-    system_prompt = (
+def _get_system_prompt(): 
+    return (
         "You are an agent equipped with a set of MCP tools. Use these tools to accurately fulfill user requests.\n\n"
         "Each tool has a specific function signature, input requirements, and output format. Read them carefully before selecting and invoking a tool.\n\n"
         "- Always choose the most relevant tool based on the task.\n"
@@ -86,30 +114,55 @@ async def create_agent(config: dict, llm=None) -> BaseTool:
         "- This tool does NOT retain the output of previous calls. If chaining values (e.g., using temperature in math), you MUST explicitly pass prior outputs via `context`.\n"
     )
 
-    # 1. Create the specialized agent executor
-    mcp_agent_executor = await create_mcp_react_agent(config, llm)
-    print("\n--- Agent Executor Created ---")
 
-    # 2. Wrap the agent executor in a StructuredTool
-    name = config["name"]
-    description = config["description"]
-    agent_tool = create_agent_as_tool(
-        agent_executor=mcp_agent_executor,
-        system_prompt=system_prompt,
-        tool_name=name,
-        tool_description=description
-    )
-    print("\n--- Agent Wrapped as a Tool ---")
-    print(f"Tool Name: {agent_tool.name}")
-    print(f"Tool Description: {agent_tool.description}")
-    print(f"Tool Args: {list(agent_tool.args.keys())}")
+async def _get_tools(name: str, mcp_config: dict) -> BaseTool:
+    """
+    Gets the MCP tool list from the MCP server with mcp_config
+    """
+    return await MultiServerMCPClient({
+        name: {
+            "url": mcp_config["url"],
+            "transport": mcp_config.get("transport", "streamable-http")
+        },
+    }).get_tools()
 
-    return agent_tool
+def _generate_tool_description(tool: StructuredTool) -> str:
+    """
+    Generate the descrtiption of the tool given as an argument.
+    TODO: Check the failure to get the info of parameters from the tool. However LLM can call a tool 
+    with proper arguments without its parameter info
+    """
+    print(f"Generating description for tool: {tool.name}")
+    
+    sig = tool.args_schema if tool.args_schema else tool.func.__annotations__
+    type_hints = get_type_hints(tool.func) if tool.func else get_type_hints(tool.coroutine)
+    return_type = type_hints.get("return", "Unknown")
+    doc = tool.description.strip() if tool.description else ""
+    func_name = tool.name
+
+    lines = []
+    lines.append(f"{func_name}(...) -> {return_type.__name__ if hasattr(return_type, '__name__') else str(return_type)}:")
+    lines.append(f" - {doc if doc else 'Performs the function defined by this tool.'}")
+
+    # 인자 설명
+    
+    if hasattr(sig, "__annotations__"):
+        for name, typ in sig.__annotations__.items():
+            typename = typ.__name__ if hasattr(typ, '__name__') else str(typ)
+            lines.append(f" - `{name}`: {typename} type input.")
+            if name == "context" and "list" in typename.lower():
+                lines.append(" - You can optionally provide a list of strings as `context` to help the tool operate correctly.")
+    else:
+        lines.append(" - (No input schema found)")
+
+    return "\n".join(lines)
 
 
+def _generate_prompt_for_tools(tools: List[BaseTool]):
+    system_prompt = _get_system_prompt()
+    tool_description = [_generate_tool_description(tool) for tool in tools]
+    return system_prompt + "\n\n" + "\n\n".join(tool_description)
 
-def get_agent_client(config: dict, llm=None) -> BaseTool:
-    return asyncio.run(create_agent(config, llm))
 
 
 def _get_test_weather_agent_config():
@@ -173,6 +226,9 @@ async def _atest_knox_mail_agent(agent):
 
 
 def _test():
+    """
+    Tests the mcp agent. The GEMINI_API_KEY environment variable must be set to work.
+    """
     llm = ChatGoogleGenerativeAI(model="gemini-2.0-flash-lite", temperature=0)
     weather_agent_config = _get_test_weather_agent_config()
     weather_agent = get_agent_client(weather_agent_config, llm)
